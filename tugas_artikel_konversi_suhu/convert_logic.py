bee057b76a6f2b16ed7d2d442488f1bdfbea51fb
Ryuugamine <yoerdanatan@gmail.com>
2023-11-26 15:15:32 +0700

    delete file

--- tugas_artikel_konversi_suhu/convert_logic.py
+++
@@ -1,7 +0,0 @@
-def celcius(input_suhu):
-        fahrenheit = (input_suhu * 9/5) + 32
-        kelvin = input_suhu + 273.15
-        return {
-                fahrenheit : fahrenheit,
-                kelvin : kelvin
-        }
